c6bc986efa58a99426c6afa74b046a28dde050f9
krystiandomagala <79994186+krystiandomagala@users.noreply.github.com>
2023-11-30 11:09:32 +0100

    limit, modal

--- team-flow/src/components/common/ChatWindow.js
+++ team-flow/src/components/common/ChatWindow.js
@@ -1,41 +1,66 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import AvatarMid from './AvatarMid';
 import ChatInput from './ChatInput';
 import Message from './Message';
 import { useChat } from '../../contexts/ChatContext';
-import { onSnapshot, doc} from 'firebase/firestore';
+import { collection,doc, query, orderBy, limit, onSnapshot } from 'firebase/firestore';
 import { db } from '../../firebase';
 
 export default function ChatWindow() {
   const [messages, setMessages] = useState([]);
-  const {data} = useChat();
-
+  const { data } = useChat();
+  const ref = useRef()
+  
+  useEffect(() => {
+    if (!data.chatId) return;
+  
+    const chatDocRef = doc(db, 'chats', data.chatId);
+  
+    const unsub = onSnapshot(chatDocRef, (docSnapshot) => {
+      if (docSnapshot.exists()) {
+        const chatData = docSnapshot.data();
+        const messagesArray = chatData.messages || [];
+        const lastFiveMessages = messagesArray.slice(-20); // Pobierz ostatnie 5 wiadomości i odwróć kolejność
+        setMessages(lastFiveMessages);
+      } else {
+        console.log('No such document!');
+      }
+    }, error => {
+      console.error('Error getting document:', error);
+    });
+  
+    return () => {
+      unsub();
+    };
+  }, [data.chatId]);
 
   useEffect(() => {
-    const unsub = onSnapshot(doc(db, "chats", data.chatId), (doc) => {
-      doc.exists() && setMessages(doc.data().messages)
-    })
+    ref.current?.scrollIntoView({behavior: 'smooth'})
+  }, [messages])
 
-    return () => {
-      unsub()
-    }
-  }, [data.chatId])
+
   return (
     <div className='p-5 w-100 d-flex flex-column'>
-      <div className='d-flex align-items-center'>
-        <AvatarMid userId={data.user?.uid}/>
-        <div className='lh-sm ms-3'>
-          <div className='chat-item-title' style={{ fontSize: '22px' }}>
-            {data.user?.fullName}
+      {data.chatId && data.chatId !== 'null' && (
+        <>
+          <div className='d-flex align-items-center'>
+            <AvatarMid userId={data.user?.uid} />
+            <div className='lh-sm ms-3'>
+              <div className='chat-item-title' style={{ fontSize: '22px' }}>
+                {data.user?.fullName}
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-      <div className='flex-grow-1 pe-3 my-3 messages-container'>
-      {messages.map(m=>(
-        <Message message={m} key={m.id} />
-      ))}
-      </div>
-        <ChatInput />
+          <div className='flex-grow-1 pe-3 my-3 messages-container'>
+            {messages.map((m) => (
+              <Message message={m} key={m.id} />
+            ))}
+
+            <div ref={ref} />
+          </div>
+          <ChatInput />
+        </>
+      )}
     </div>
   );
 }
--- team-flow/src/components/common/Message.js
+++ team-flow/src/components/common/Message.js
@@ -1,26 +1,37 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useAuth } from '../../contexts/AuthContext';
 import Avatar from '../common/Avatar';
-export default function Message({ message }) {
-  const { currentUser } = useAuth();
-  const ref = useRef()
+import { Modal } from 'react-bootstrap';
 
-  useEffect(() => {
-    ref.current?.scrollIntoView({ behavior: "smooth" }); 
-  }, [message]);
+
+export default function Message({ message  }) {
+  const { currentUser } = useAuth();
+  const [showModal, setShowModal] = useState(false);
+
+  const handleImageClick = () => {
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
 
   return (
     <>
-      <div ref={ref} className={`d-flex gap-3 align-items-end my-2 ${ message.senderId === currentUser.uid ? 'owner' : ''}`}>
+      <div  className={`d-flex gap-3 align-items-end my-2 ${message.senderId === currentUser.uid ? 'owner' : ''}`}>
         <Avatar userId={message.senderId} />
-        {
-          message.image ? ( <img
-          className='message-img'
-          src={message.image}
-        />) : (
-        <div className='message-content px-3 py-2'>{message.text}</div>
-        )
-        }
+        {message.image ? (
+          <>
+            <img className='message-img' src={message.image} onClick={handleImageClick} style={{ cursor: 'pointer' }} onLoad={console.log("loaded")}/>
+            <Modal show={showModal} onHide={handleCloseModal} size="xl" centered>
+              <Modal.Body>
+                <img src={message.image} className='img-fluid' alt='Chat Image' />
+              </Modal.Body>
+            </Modal>
+          </>
+        ) : (
+          <div className='message-content px-3 py-2'>{message.text}</div>
+        )}
       </div>
     </>
   );
