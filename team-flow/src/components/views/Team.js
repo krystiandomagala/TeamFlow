874292608767c518b2ac4b87a204a60a6f8f9a8f
krystiandomagala <79994186+krystiandomagala@users.noreply.github.com>
2023-12-11 20:26:38 +0100

    accesscode

--- team-flow/src/components/views/Team.js
+++ team-flow/src/components/views/Team.js
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import MainLayout from '../../layouts/MainLayout';
 import Loading from '../common/Loading'
 import useTeamExists from '../../hooks/useTeamExists';
+import { useUserTeamData } from '../../contexts/TeamContext';
 
 export default function Team() {
   const { isLoading } = useTeamExists();
+  const { getTeamData } = useUserTeamData()
+  const { teamId } = useTeamExists()
+  const [accessCode, setAccessCode] = useState('')
+
+  useEffect(() => {
+    getTeamData(teamId).then((data) => setAccessCode(data.accessCode))
+  }, [teamId])
+
 
   if (isLoading) {
-    return <MainLayout><Loading/></MainLayout>;
+    return <MainLayout><Loading /></MainLayout>;
   }
 
   return (
     <MainLayout>
-      <div>Team</div>
+      access code: {accessCode}
     </MainLayout>
   );
 }
